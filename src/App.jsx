8eabd682e531a4750f5176b4ffbbe90f418df0a4
Blladerunner <rufatahmedik@gmail.com>
2025-03-19 22:39:00 +0100

    adding and style main image/background

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ function App() {
   return (
     <>
       <h1>8-Ball Pool Game</h1>
+      <img
+        className="main-image"
+        src={"./src/assets/sprites/spr_background4.png"}
+        style={{ margin: "0px -200px" }}
+      />
     </>
   );
 }
